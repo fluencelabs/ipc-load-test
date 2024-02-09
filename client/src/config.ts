1feac460453791e79683012a0fd354131b5626fa
InversionSpaces <inversionspaces@vivaldi.net>
2024-02-09 15:37:29 +0000

    Register a few providers

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,20 +1,21 @@
 import fs from "fs";
 import type { BytesLike } from "ethers";
 
-export interface Config {
-  provider_sk: string;
-  provider_name: string;
-  test_rpc_url: string;
-  unit_id?: BytesLike | undefined;
-}
+export interface PeerConfig {
+  cu_count: number;
+  cu_ids: BytesLike[];
+};
 
-const isValidConfig = (config: any): config is Config => {
-  return (
-    typeof config.provider_sk === "string" &&
-    typeof config.provider_name === "string" &&
-    typeof config.test_rpc_url === "string" &&
-    (typeof config.unit_id === "string" || config.unit_id === undefined)
-  );
+export interface ProviderConfig {
+  name: string;
+  sk: string | undefined;
+  peers: PeerConfig[];
+};
+
+export interface Config {
+  test_rpc_url: string;
+  default_sk: string | undefined;
+  providers: ProviderConfig[];
 };
 
 export const loadConfig = (filePath: string): Config => {
@@ -22,8 +23,21 @@ export const loadConfig = (filePath: string): Config => {
     const rawData = fs.readFileSync(filePath, "utf8");
     const config = JSON.parse(rawData);
 
-    if (!isValidConfig(config)) {
-      throw new Error(`Invalid config file format: ${config}`);
+    config.providers = config.providers || [];
+    config.providers.forEach((provider: ProviderConfig) => {
+      provider.peers = provider.peers || [];
+      provider.peers.forEach((peer: PeerConfig) => {
+        peer.cu_ids = peer.cu_ids || [];
+      });
+    });
+
+    const isDefaultSkSet = config.default_sk !== undefined;
+    const areAllProviderSkSet = config.providers.every(
+      (provider: ProviderConfig) => provider.sk !== undefined
+    );
+
+    if (!isDefaultSkSet && !areAllProviderSkSet) {
+      throw new Error('Either default_sk must be set, or all provider sk must be set.');
     }
 
     return config;
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,17 +1,41 @@
 import { ethers } from "ethers";
 import { DealClient } from "@fluencelabs/deal-ts-clients";
 
-import { loadConfig, saveConfig } from "./config.js";
-import { delay } from "./utils.js";
+import { loadConfig, saveConfig, type Config, type ProviderConfig } from "./config.js";
+// import { delay } from "./utils.js";
 
 const DEFAULT_CONFIRMATIONS = 1;
 
-async function getDefaultOfferFixture(
-  owner: string,
+// NOTE: Modifies provider config in place
+async function setupOffer(
+  provider: ProviderConfig,
+  address: string,
   paymentToken: string,
   timestamp: number
 ) {
-  const offerFixture = {
+  // Setup cu_ids for each peer.
+  provider.peers.forEach((peer, pid) => {
+    peer.cu_ids = [];
+    for (let i = 0; i < peer.cu_count; i++) {
+      peer.cu_ids.push(
+        ethers.encodeBytes32String(
+          `${provider.name}:p${pid}:u${i}:${timestamp}`
+        )
+      );
+    }
+  });
+
+  const peers = provider.peers.map((peer, pid) => {
+    return {
+      peerId: ethers.encodeBytes32String(
+        `${provider.name}:p${pid}:${timestamp}`
+      ),
+      unitIds: peer.cu_ids,
+      owner: address,
+    };
+  });
+
+  return {
     minPricePerWorkerEpoch: ethers.parseEther("0.01"),
     paymentToken: paymentToken,
     effectors: [
@@ -20,145 +44,150 @@ async function getDefaultOfferFixture(
         hash: ethers.encodeBytes32String("TestEffectorHash"),
       },
     ],
-    peers: [
-      {
-        peerId: ethers.encodeBytes32String(`peerId0:${timestamp}`),
-        unitIds: [ethers.encodeBytes32String(`unitId0:${timestamp}`)],
-        owner: owner,
-      },
-    ],
+    peers,
   };
+}
 
-  return offerFixture;
+async function registerProvider(
+  config: Config,
+  provider: ProviderConfig,
+) {
+  console.log("Registering provider: ", provider.name, "...")
+
+  const sk = (provider.sk || config.default_sk)!;
+  const rpc = new ethers.JsonRpcProvider(config.test_rpc_url);
+  const signer = new ethers.Wallet(sk, rpc);
+  const client = new DealClient(signer, "local");
+
+  console.log("Getting USDC token address...");
+  const paymentToken = await client.getUSDC();
+  const paymentTokenAddress = await paymentToken.getAddress();
+
+  console.log("Getting market contract...");
+  const market = await client.getMarket();
+
+  console.log("Getting signer address...");
+  const signerAddress = await signer.getAddress();
+
+  console.log("Getting block number and timestamp...");
+  const blockNumber = await rpc.getBlockNumber();
+  const timestamp = (await rpc.getBlock(blockNumber))!.timestamp;
+
+  console.log("Timestamp:", timestamp, "Block number:", blockNumber, "...");
+
+  const offer = await setupOffer(
+    provider,
+    signerAddress,
+    paymentTokenAddress,
+    timestamp
+  );
+
+  console.log("Setting provider info...");
+  const setProviderInfoTx = await market.setProviderInfo(provider.name, {
+    prefixes: "0x12345678",
+    hash: ethers.encodeBytes32String(`${provider.name}:${timestamp}`),
+  });
+  await setProviderInfoTx.wait(DEFAULT_CONFIRMATIONS);
+
+  console.log("Registering market offer...");
+  const registerMarketOfferTx = await market.registerMarketOffer(
+    offer.minPricePerWorkerEpoch,
+    offer.paymentToken,
+    offer.effectors,
+    offer.peers
+  );
+  await registerMarketOfferTx.wait(DEFAULT_CONFIRMATIONS);
+
+  const capacity = await client.getCapacity();
+  const capacityMinDuration = await capacity.minDuration();
+
+  for (const peer of offer.peers) {
+    // bytes32 peerId, uint256 duration, address delegator, uint256 rewardDelegationRate
+    console.log(
+      "Create commitment for peer: ",
+      peer.peerId,
+      " with duration: ",
+      capacityMinDuration,
+      "..."
+    );
+
+    const createCommitmentTx = await capacity.createCommitment(
+      peer.peerId,
+      capacityMinDuration,
+      signerAddress,
+      1
+    );
+    await createCommitmentTx.wait(DEFAULT_CONFIRMATIONS);
+  }
+
+  console.log("Get capacity commitment created events...");
+  // Fetch created commitmentIds from chain.
+  const filterCreatedCC = capacity.filters.CommitmentCreated;
+
+  const capacityCommitmentCreatedEvents = await capacity.queryFilter(
+    filterCreatedCC,
+    blockNumber
+  );
+  const capacityCommitmentCreatedEventsLast = capacityCommitmentCreatedEvents
+    .reverse()
+    .slice(0, offer.peers.length);
+  console.log(
+    "Got",
+    capacityCommitmentCreatedEventsLast.length,
+    "capacity commitment created events for",
+    offer.peers.length,
+    "peers..."
+  );
+
+  const commitmentIds = capacityCommitmentCreatedEventsLast.map(
+    (event) => event.args.commitmentId
+  );
+
+  let totalCollateral = BigInt(0);
+  for (const commitmentId of commitmentIds) {
+    const commitment = await capacity.getCommitment(commitmentId);
+    const collateralToApproveCommitment =
+      commitment.collateralPerUnit * commitment.unitCount;
+    console.log(
+      "Collateral for commitmentId:",
+      commitmentId,
+      "=",
+      collateralToApproveCommitment
+    );
+    totalCollateral += collateralToApproveCommitment;
+  }
+
+  console.log("Depositing collateral", totalCollateral, "...");
+  capacity.depositCollateral
+  const depositCollateralTx = await capacity.depositCollateral(commitmentIds, {
+    value: totalCollateral,
+  });
+  await depositCollateralTx.wait(DEFAULT_CONFIRMATIONS);
+
+  // const filterActivatedCC = capacity.filters.CommitmentActivated();
+  // console.info("Waiting for commitment activated event...");
+  // for (let i = 0; i < 10000; i++) {
+  //   const capacityCommitmentActivatedEvents = await capacity.queryFilter(
+  //     filterActivatedCC,
+  //     blockNumber
+  //   );
+
+  //   if (capacityCommitmentActivatedEvents.length > 0) {
+  //     console.info("Got commitment activated event...");
+  //     break;
+  //   } else {
+  //     console.info("No commitment activated event...");
+  //   }
+
+  //   await delay(1000);
+  // }
 }
 
 const config = loadConfig("config.json");
 
-const rpc = new ethers.JsonRpcProvider(config.test_rpc_url);
-const signer = new ethers.Wallet(config.provider_sk, rpc);
-const client = new DealClient(signer, "local");
-
-console.log("Getting USDC token address...");
-const paymentToken = await client.getUSDC();
-const paymentTokenAddress = await paymentToken.getAddress();
-
-console.log("Getting market contract...");
-const market = await client.getMarket();
-
-console.log("Getting signer address...");
-const signerAddress = await signer.getAddress();
-
-console.log("Getting block number and timestamp...");
-const blockNumber = await rpc.getBlockNumber();
-const timestamp = (await rpc.getBlock(blockNumber))!.timestamp;
-
-const offer = await getDefaultOfferFixture(
-  signerAddress,
-  paymentTokenAddress,
-  timestamp
-);
-
-console.log("Setting provider info...");
-const setProviderInfoTx = await market.setProviderInfo(config.provider_name, {
-  prefixes: "0x12345678",
-  hash: ethers.encodeBytes32String(`${config.provider_name}:${timestamp}`),
-});
-await setProviderInfoTx.wait(DEFAULT_CONFIRMATIONS);
-
-console.log("Registering market offer...");
-const registerMarketOfferTx = await market.registerMarketOffer(
-  offer.minPricePerWorkerEpoch,
-  offer.paymentToken,
-  offer.effectors,
-  offer.peers
-);
-await registerMarketOfferTx.wait(DEFAULT_CONFIRMATIONS);
-
-const capacity = await client.getCapacity();
-const capacityMinDuration = await capacity.minDuration();
-
-for (const peer of offer.peers) {
-  // bytes32 peerId, uint256 duration, address delegator, uint256 rewardDelegationRate
-  console.log(
-    "Create commitment for peer: ",
-    peer.peerId,
-    " with duration: ",
-    capacityMinDuration,
-    "..."
-  );
-
-  const createCommitmentTx = await capacity.createCommitment(
-    peer.peerId,
-    capacityMinDuration,
-    signerAddress,
-    1
-  );
-  await createCommitmentTx.wait(DEFAULT_CONFIRMATIONS);
-}
-
-console.log("Get capacity commitment created events...");
-// Fetch created commitmentIds from chain.
-const filterCreatedCC = capacity.filters.CommitmentCreated;
-
-const capacityCommitmentCreatedEvents = await capacity.queryFilter(
-  filterCreatedCC,
-  blockNumber
-);
-const capacityCommitmentCreatedEventsLast = capacityCommitmentCreatedEvents
-  .reverse()
-  .slice(0, offer.peers.length);
-console.log(
-  "Got",
-  capacityCommitmentCreatedEventsLast.length,
-  "capacity commitment created events for",
-  offer.peers.length,
-  "peers..."
-);
-
-const commitmentIds = capacityCommitmentCreatedEventsLast.map(
-  (event) => event.args.commitmentId
-);
-
-let totalCollateral = BigInt(0);
-for (const commitmentId of commitmentIds) {
-  const commitment = await capacity.getCommitment(commitmentId);
-  const collateralToApproveCommitment =
-    commitment.collateralPerUnit * commitment.unitCount;
-  console.log(
-    "Collateral for commitmentId:",
-    commitmentId,
-    "=",
-    collateralToApproveCommitment
-  );
-  totalCollateral += collateralToApproveCommitment;
-}
-
-console.log("Depositing collateral", totalCollateral, "...");
-capacity.depositCollateral
-const depositCollateralTx = await capacity.depositCollateral(commitmentIds, {
-  value: totalCollateral,
-});
-await depositCollateralTx.wait(DEFAULT_CONFIRMATIONS);
-
-const filterActivatedCC = capacity.filters.CommitmentActivated();
-console.info("Waiting for commitment activated event...");
-for (let i = 0; i < 10000; i++) {
-  const capacityCommitmentActivatedEvents = await capacity.queryFilter(
-    filterActivatedCC,
-    blockNumber
-  );
-
-  if (capacityCommitmentActivatedEvents.length > 0) {
-    console.info("Got commitment activated event...");
-    break;
-  } else {
-    console.info("No commitment activated event...");
-  }
-
-  await delay(1000);
+for (const provider of config.providers) {
+  await registerProvider(config, provider);
 }
 
 console.info("Updating config...");
-saveConfig("config.json", { ...config, unit_id: offer.peers[0]?.unitIds[0] });
-
-console.info("Unit ID: ", offer.peers[0]?.unitIds[0]);
+saveConfig("config.json", config);
