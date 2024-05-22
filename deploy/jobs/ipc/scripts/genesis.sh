b8b03b19e5ba1ead98d006159282760f1cf49081
Anatoly Laskaris <github_me@nahsi.dev>
2024-05-22 23:44:07 +0300

    Update

--- deploy/jobs/ipc/scripts/genesis.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-docker run --rm -e FM_NETWORK=test --user ${UID} -v ./keys:/keys -v ./files:/files -v ./scripts:/scripts --entrypoint /scripts/entrypoint/fendermint.sh ghcr.io/consensus-shipyard/fendermint
--- deploy/main.tf
+++
@@ -1,9 +0,0 @@
-data "digitalocean_ssh_key" "key" {
-  name       = "default-ssh-key"
-}
-
-data "digitalocean_droplet_snapshot" "snapshot" {
-  name_regex  = "ephemeral-ubuntu-22-04-.*"
-  region      = "fra1"
-  most_recent = true
-}
