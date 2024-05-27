03b55200251082242a0e1a0383af83678c814a27
Anatoly Laskaris <github_me@nahsi.dev>
2024-05-27 19:48:34 +0300

    chore: Better servers

--- dev-env/servers.tf
+++ dev-env/servers.tf
@@ -8,7 +8,7 @@ resource "digitalocean_droplet" "server" {
   for_each = { for index, name in local.server : name => index }
 
   name      = "${terraform.workspace}-${each.key}"
-  size      = "s-2vcpu-4gb"
+  size      = "s-4vcpu-8gb"
   image     = var.snapshot
   region    = var.region
   user_data = templatefile("${path.module}/files/server.sh", { workspace = terraform.workspace })
