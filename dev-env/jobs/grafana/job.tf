f3f122aa282bb498752b17c904bbfde5a2c0e032
Anatoly Laskaris <github_me@nahsi.dev>
2024-05-27 16:42:01 +0300

    Wait for healthy status

--- dev-env/jobs/grafana/job.tf
+++ dev-env/jobs/grafana/job.tf
@@ -14,6 +14,8 @@ resource "nomad_job" "grafana" {
 
   jobspec          = file("${path.module}/job.nomad.hcl")
   purge_on_destroy = true
+  detach           = false
+  rerun_if_dead    = true
 
   hcl2 {
     vars = {
