fd822573a64cf5cb17013ebe9956501de4a6f8b4
Anatoly Laskaris <github_me@nahsi.dev>
2024-06-05 10:48:08 +0300

    Add dashboards

--- dev-env/jobs/default/grafana/backend.tf
+++ dev-env/jobs/default/grafana/backend.tf
@@ -26,6 +26,6 @@ provider "postgresql" {
   database         = "postgres"
   username         = "postgres"
   password         = "postgres"
-  superuser        = true
+  superuser        = false
   sslmode          = "disable"
 }
