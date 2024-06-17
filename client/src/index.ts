d8d8e846cd7db9dcf6e7f5bb44f19b347bcc5ec7
InversionSpaces <inversionspaces@vivaldi.net>
2024-06-17 14:54:38 +0000

    Bump

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,13 +1,13 @@
 import { bench } from "./bench.js";
 
-const interval = 3000;
+const interval = 1600;
 const duration = 20 * 60 * 1000;
 const batches = Math.floor(duration / interval)
 await bench({
   interval: interval,
-  cusNumber: 4,
+  cusNumber: 8,
   sendersNumber: batches,
-  batchSize: 16,
+  batchSize: 64,
   batchesToSend: batches,
   configPath: "./config.json",
   metricsPath: "./metrics.json",
