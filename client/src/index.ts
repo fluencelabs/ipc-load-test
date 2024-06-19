561708023e5c2fba94a5e39708d9009b9b9567d3
InversionSpaces <inversionspaces@vivaldi.net>
2024-06-19 09:16:02 +0000

    Set parameters

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,12 +2,14 @@ import { bench } from "./bench.js";
 
 const interval = 3200;
 const duration = 60 * 60 * 1000;
+const cus = 8;
+const proofs = 16;
 const batches = Math.floor(duration / interval)
 await bench({
   interval: interval,
-  cusNumber: 8,
+  cusNumber: cus,
   sendersNumber: batches,
-  proofsPerCu: 16,
+  proofsPerCu: proofs,
   batchesToSend: batches,
   configPath: "./config.json",
   metricsPath: "./metrics.json",
