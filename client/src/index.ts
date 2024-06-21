256c1f13f5fb4965f11d515f895bc15b36bdd226
InversionSpaces <inversionspaces@vivaldi.net>
2024-06-21 09:04:42 +0000

    Bump

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,7 +2,7 @@ import { bench } from "./bench.js";
 
 for (const p of [8]) {
   for (const b of [64]) {
-    for (const c of [4, 8]) {
+    for (const c of [4]) {
       await bench(4, p, c, b, `./metrics_12n_${p}p_${b}b_${c}cu.json`);
     }
   }
